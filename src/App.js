470fbe4e95db3ce0adeabf69df8de8ee4446e6be
Fernando Monteiro <fernando.gomesrj@gmail.com>
2021-10-03 16:46:37 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  const projects = ['Desenvolvimento de app', 'FrontEndWeb'];
+
+
+  return  (
+    <>  
+    <Header title="Project">
+    </Header>
+    </>
+  );
+}
+
+export default App;
